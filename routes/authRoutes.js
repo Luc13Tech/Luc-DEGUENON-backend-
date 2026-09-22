82fbe8882eb660ed78d263935cbf866558a24aee
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-22 14:38:35 +0000

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const router = express.Router();
-const { loginAdmin, logoutAdmin, getMe } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
 
+const router = express.Router();
+
+const {
+  loginAdmin,
+  logoutAdmin,
+  getMe,
+} = require('../controllers/authController');
+
+const {
+  protect,
+} = require('../middleware/authMiddleware');
+
+/*
+ * Connexion administrateur
+ */
 router.post('/login', loginAdmin);
+
+/*
+ * Déconnexion administrateur
+ */
 router.post('/logout', logoutAdmin);
+
+/*
+ * Vérification de la session administrateur
+ */
 router.get('/me', protect, getMe);
 
 module.exports = router;
