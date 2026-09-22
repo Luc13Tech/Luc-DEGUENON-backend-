cc48a0ad8951a3b9b188f0eddab4063a64b684f4
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-22 14:28:18 +0000

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,26 @@
 const cloudinary = require('cloudinary').v2;
 
+const requiredVariables = [
+  'CLOUDINARY_CLOUD_NAME',
+  'CLOUDINARY_API_KEY',
+  'CLOUDINARY_API_SECRET',
+];
+
+const missingVariables = requiredVariables.filter(
+  (variable) => !process.env[variable]
+);
+
+if (missingVariables.length > 0) {
+  console.warn(
+    `Variables Cloudinary manquantes : ${missingVariables.join(', ')}`
+  );
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 module.exports = cloudinary;
